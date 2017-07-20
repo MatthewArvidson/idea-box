fa36410713b302335bd4414ddce26e4c31f40813
Jorge Ed PerezGa <perezga@icloud.com>
2017-07-20 09:11:32 -0600

    Voting buttons

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 var submit = document.querySelector(".save-button");
+var sectionBottom = $(".section-bottom");
+var search = $(".search-engine")
 
-var sectionBottom = document.querySelector(".section-bottom")
 
-submit.addEventListener("click", function(){
-  event.preventDefault();
-  var title = document.querySelector(".input-title").value;
-  var body = document.querySelector(".input-body").value;
-  console.log(body)
-  var theIdea = new Card(title, body);
+// submit.addEventListener("click", function(){
+//   event.preventDefault();
+//   var title = document.querySelector(".input-title").value;
+//   var body = document.querySelector(".input-body").value;
+//   console.log(body)
+//   var theIdea = new Card(title, body);
+//   idea(theIdea);
+// })
 
-  idea(theIdea)
-})
+submit.addEventListener("click", createIdea)
+
+$(document).ready(loadPage)
+
+//
+
+search.on("keydown", filterIdeas)
+
+
+function filterIdeas(){
+  sectionBottom.html("")
+  var searchVal= $(this).val();
+  var ideas = getFromLocal();
+  // console.log(searchVal);
+// console.log($(this).val())
+  var filterIdeas = ideas.filter(function(idea, index){
+  // console.log(idea);
+  // console.log(index);
+  return searchVal === idea.title || searchVal === idea.body
+  })
+ filterIdeas.forEach(function(idea){
+   appendIdea(idea);
+ })
+}
+
 
 // function createCard(){
 //   event.preventDefault();
@@ -23,23 +49,48 @@ function Card(title, body){
   this.title = title;
   this.body = body;
   this.id = Date.now();
-  this.quality = "swell";
+  this.quality = "swill";
 }
 
-function idea(theIdea){
-$('.section-bottom').append(`
-  <article id= ${theIdea.id}>
-    <h2 class="idea-title">${theIdea.title}</h2>
-    <img class="delete" src="Imagery/delete.svg">
-    <p class="description">${theIdea.body}</p>
-    <img class="up" src="Imagery/upvote.svg">
-    <img class="down" src="Imagery/downvote.svg">
-    <p class="quality">quality: </p><p class="swell">${theIdea.quality}</p>
-  <hr class="line">
-</article>
+function appendIdea(theIdea){
+$('.section-bottom').prepend(`
+  <article class="container" id=${theIdea.id}>
+    <h2 contenteditable=true class="idea-title">${theIdea.title}</h2>
+    <button class="delete"></button>
+    <p contenteditable=true class="description">${theIdea.body}</p>
+    <button class="up"></button>
+    <button class="down"></button>
+    <p class="quality">quality: </p><p class="level">${theIdea.quality}</p>
+    <hr class="line">
+  </article>
 `)}
 
+sectionBottom.on("click", ".delete", function(){
+$(this).parent().remove()
+});
 
+//sectionBottom.on("click", ".delete", function(event){
+//$(event.target).parent().remove()
+
+sectionBottom.on("click", ".up", function(){
+  var level = $(this).parent().find(".level").text();
+  if(level === "swill"){
+    $(this).parent().find(".level").text("plausible")
+  }
+  else if(level === "plausible"){
+    $(this).parent().find(".level").text("genius")
+  }
+  });
+
+sectionBottom.on("click", ".down", function(){
+  var level = $(this).parent().find(".level").text();
+  if(level === "genius"){
+    $(this).parent().find(".level").text("plausible")
+  }
+  else if(level === "plausible"){
+    $(this).parent().find(".level").text("swill")
+  }
+});
 
 // function addCardToPage(cardCreate){
 //   var newArticle = document.createElement("li");
@@ -50,3 +101,30 @@ $('.section-bottom').append(`
 //                           "</article>";
 //   sectionBottom.appendChild(newArticle);
 // }
+function loadPage(){
+  for (var i = 0; i < localStorage.length; i++) {
+  appendIdea(JSON.parse(localStorage.getItem(localStorage.key(i))))
+  }
+}
+
+
+function saveToLocal(idea){
+  localStorage.setItem(idea.id, JSON.stringify(idea))
+}
+ function getFromLocal(){
+ var ideas = [];
+ for (var i = 0; i < localStorage.length; i++) {
+ ideas.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+ }
+ return ideas
+}
+
+function createIdea(event){
+  event.preventDefault();
+  var title = $(".input-title").val();
+  var body = $(".input-body").val();
+  var theIdea = new Card(title, body);
+  appendIdea(theIdea);
+  saveToLocal(theIdea);
+  //reset input fields
+}
